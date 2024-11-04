d2728bebe81380aa61910ad65312a7be39d175a0
Abhinav_p5 <abhinavpappu05@gmail.com>
2024-11-03 20:32:02 -0500

    added functinos and plot to plots the peak_iffs for the aggregated vf data

--- vf_popul_model.py
+++ vf_popul_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import logging
 from lmfit import minimize, fit_report, Parameters
 from aim2_population_model_spatial_aff_parallel import get_mod_spike
@@ -29,24 +30,21 @@ class VF_Population_Model:
     def __init__(self, vf_tip_size, aff_type):
         self.vf_tip_size = vf_tip_size
         self.aff_type = aff_type
-        self.results
+        self.results = None
 
-        
-        
 
     """
         functino takes in a vf_tip_size (given that that there is data assicated with it) an
         afferent type, and runs the single unit model for all of those coordinates with the data
         
     """
-
     def spatial_stress_vf_model(self, scaling_factor = 0.3):
 
         #reading data in 
         coords = pd.read_csv(f"data/vfspatial/{self.vf_tip_size}_spatial_coords.csv", header = None)
         x_coords = coords[0]
         y_coords = coords[1]
-        stress_data = pd.read_csv(f"data/vfspatial/3.61_spatial_stress.csv")
+        stress_data = pd.read_csv(f"data/vfspatial/3.61_spatial_stress.csv", )
         time = stress_data['Time (ms)'].to_numpy()
 
         afferent_type = []
@@ -59,11 +57,15 @@ class VF_Population_Model:
         last_spike_time = []
 
         #iterating through each of the coordinates
-        for i, row in coords.iterrows():
+        for i, row in coords.iloc[1:].iterrows():
+            
 
             #getting stress data
-            stress = stress_data[f"Coord {i+1} Stress (kPa)"]
-
+            
+            if f"Coord {i} Stress (kPa)" in stress_data.columns:
+                stress = stress_data[f"Coord {i} Stress (kPa)"]
+            else:
+                logging.warning("STRESS VALUE COULD NOT BE INDEXED")
 
             lmpars = lmpars_init_dict['t3f12v3final']
             if afferent_type == "RA":
@@ -96,12 +98,13 @@ class VF_Population_Model:
             afferent_type.append(self.aff_type)
             x_pos.append(row[0])
             y_pos.append(row[1])
-            spikes.append(len(mod_spike_time) if mod_spike_time !=0 else None)
+            spikes.append(len(mod_spike_time) if len(mod_spike_time) !=0 else None)
             mean_firing_frequency.append(features["Average Firing Rate"])
             peak_firing_frequency.append(np.max(mod_fr_inst_interp))
             first_spike_time.append(mod_spike_time[0] if len(mod_spike_time) != None else None)
             last_spike_time.append(mod_spike_time[-1])
 
+            
         model_results = {
             'afferent_type': self.aff_type,
             'x_position': x_pos,
@@ -113,12 +116,45 @@ class VF_Population_Model:
             'last_spike_time' : last_spike_time
         }
 
+        self.results = model_results
         return model_results
 
-
     def aggregate_results(self):
+        df = pd.DataFrame(self.results)
+        file_path = f"generated_csv_files/{self.vf_tip_size}_vf_popul_model.csv"
+        df.to_csv(file_path, index = False)
+        return file_path 
+    
+    def plot(self):
+        #colors for differnet afferents
+        colors = {'SA': '#31a354', 'RA': '#3182bd'}
+        plt.figure(figsize=(10, 5))
 
+        # Plot the stimulus locations as circles
+        x_positions = self.results.get("x_position")
+        y_positions = self.results.get("y_position")
+        mean_iffs = self.results.get("mean_firing_frequency")
+        peak_iffs = self.results.get("peak_firing_frequency")
+        
+        x_positions = [float(value) for value in x_positions]
+        y_positions = [float(value) for value in y_positions]
+        #scaling peak_iffs so it looks better when plotting
+    
+        alphas = [float(value)/max(peak_iffs) for value in peak_iffs]
 
+        #Scatter plot
+                # Plot the stimulus locations as circles
+        for x_pos, y_pos, radius, alpha in zip(x_positions, y_positions, peak_iffs, alphas):
+            plt.gca().add_patch(
+                patches.Circle((x_pos, y_pos), radius*2, edgecolor='black', facecolor = colors.get(self.aff_type) , linewidth=1, alpha = 0.5)
+            )
+        plt.xlabel('Length (mm)')
+        plt.ylabel('Width (mm)')
+        plt.title("VF Afferent Stress Distribution")
+        plt.gca().set_aspect('equal', adjustable='box')
+        plt.xlim(min(x_positions) - 1, max(x_positions) + 1)
+        plt.ylim(min(y_positions) - 1, max(y_positions) + 1)
+        plt.savefig(f"vf_graphs/aggregated_results_on_grid/{self.vf_tip_size}_{self.aff_type}_constant_opacity.png")
 
 
 
@@ -130,4 +166,10 @@ if __name__ == '__main__':
 
     #runs the model which calculates the results
     vf_model.spatial_stress_vf_model()
+
+    #writing the data to csv
+    vf_model.aggregate_results()
+
+    #plotting the aggregated_results
+    vf_model.plot()
 
